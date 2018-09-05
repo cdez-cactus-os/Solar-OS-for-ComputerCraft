f3ae7284ed7cefcc002e7deb1a3589b901cb80ed
LDDestroier <etheilig02@gmail.com>
2018-09-05 13:18:45 -0400

    Changed to apemanzilla's gitget

--- install.lua
+++ install.lua
@@ -1,216 +1,146 @@
---[[
-  GitGet by LDDestroier (not apemanzilla's), modded for SolarOS
-  To install SolarOS, use this:
-  > wget https://github.com/cdez-cactus-os/Solar-OS-for-ComputerCraft/raw/master/install.lua install.lua
-  > install.lua
-                              --]]
-local verbose = true
+--[[ /gitget
+GitHub downloading utility for CC.
+Developed by apemanzilla. (thanks for that, by the way)
  
-local tArg = {...}
-local reponame = "cdez-cactus-os/Solar-OS-for-ComputerCraft"
-local repopath = "Version 1.3.9"
-local outpath = tArg[1] or ""
+This requires ElvishJerricco's JSON parsing API.
+Direct link: http://pastebin.com/raw.php?i=4nRg9CHU
+]]--
+
+-- Edit these variables to use preset mode.
+-- Whether to download the files asynchronously (huge speed benefits, will also retry failed files)
+-- If false will download the files one by one and use the old output (List each file name as it's downloaded) instead of the progress bar
+local async = true
+
+-- Whether to write to the terminal as files are downloaded
+-- Note that unless checked for this will not affect pre-set start/done code below
+local silent = false
+
+local preset = {
+	-- The GitHub account name
+	user = "cdez-cactus-os",
+	-- The GitHub repository name
+	repo = "Solar-OS-for-ComputerCraft",
+	
+	-- The branch or commit tree to download (defaults to 'master')
+	branch = "master",
+	
+	-- The local folder to save all the files to (defaults to '/')
+	path = nil,
+	
+	-- Function to run before starting the download
+	start = function()
+		if not silent then print("Downloading files from GitHub...") end
+	end,
+	
+	-- Function to run when the download completes
+	done = function(path)
+		if not silent then
+      print("Deleting unneccesary files")
+      fs.delete("README.md")
+      fs.delete("LICENCE")
+      fs.delete("_config.yml")
+      local list = fs.list(fs.combine(path, "Version 1.3.9"))
+      for a = 1, #list do
+        fs.move(fs.combine(fs.combine(path, "Version 1.3.9") list[a]), fs.combine(path, list[a]))
+      end
+      fs.delete("Version 1.3.9")
+      print("Done!")
+    end
+	end
+}
+
+-- Leave the rest of the program alone.
+local args = {...}
+
+args[1] = preset.user or args[1]
+args[2] = preset.repo or args[2]
+args[3] = preset.branch or args[3] or "master"
+args[4] = preset.path or args[4] or ""
  
-if outpath:sub(1,1) ~= "/" then
-    outpath = fs.combine(shell.dir(),outpath)
-end
- 
---thank you ElvishJerricco
-local controls = {["\n"]="\\n", ["\r"]="\\r", ["\t"]="\\t", ["\b"]="\\b", ["\f"]="\\f", ["\""]="\\\"", ["\\"]="\\\\"}
-local function isArray(t)
-    local max = 0
-    for k,v in pairs(t) do
-        if type(k) ~= "number" then
-            return false
-        elseif k > max then
-            max = k
-        end
-    end
-    return max == #t
-end
-local whites = {['\n']=true; ['\r']=true; ['\t']=true; [' ']=true; [',']=true; [':']=true}
-function removeWhite(str)
-    while whites[str:sub(1, 1)] do str = str:sub(2) end return str
-end
-local function encodeCommon(val, pretty, tabLevel, tTracking)
-    local str = ""
-    local function tab(s)
-        str = str .. ("\t"):rep(tabLevel) .. s
-    end
-    local function arrEncoding(val, bracket, closeBracket, iterator, loopFunc)
-        str = str .. bracket
-        if pretty then
-            str = str .. "\n"
-            tabLevel = tabLevel + 1
-        end
-        for k,v in iterator(val) do
-            tab("")
-            loopFunc(k,v)
-            str = str .. ","
-            if pretty then str = str .. "\n" end
-        end
-        if pretty then tabLevel = tabLevel - 1 end
-        if str:sub(-2) == ",\n" then str = str:sub(1, -3) .. "\n"
-        elseif str:sub(-1) == "," then str = str:sub(1, -2) end
-        tab(closeBracket)
-    end
-    if type(val) == "table" then
-        assert(not tTracking[val], "Cannot encode a table holding itself recursively")
-        tTracking[val] = true
-        if isArray(val) then
-            arrEncoding(val, "[", "]", ipairs, function(k,v)
-                str = str .. encodeCommon(v, pretty, tabLevel, tTracking)
-            end)
-        else
-            arrEncoding(val, "{", "}", pairs, function(k,v)
-                assert(type(k) == "string", "JSON object keys must be strings", 2)
-                str = str .. encodeCommon(k, pretty, tabLevel, tTracking)
-                str = str .. (pretty and ": " or ":") .. encodeCommon(v, pretty, tabLevel, tTracking)
-            end)
-        end
-    elseif type(val) == "string" then str = '"' .. val:gsub("[%c\"\\]", controls) .. '"'
-    elseif type(val) == "number" or type(val) == "boolean" then str = tostring(val)
-    else error("JSON only supports arrays, objects, numbers, booleans, and strings", 2) end
-    return str
-end
-local function encode(val)
-    return encodeCommon(val, false, 0, {})
-end
-local function encodePretty(val)
-    return encodeCommon(val, true, 0, {})
-end
-local decodeControls = {}
-for k,v in pairs(controls) do
-    decodeControls[v] = k
-end
-local function parseBoolean(str)
-    if str:sub(1, 4) == "true" then
-        return true, removeWhite(str:sub(5))
-    else
-        return false, removeWhite(str:sub(6))
-    end
-end
-local function parseNull(str)
-    return nil, removeWhite(str:sub(5))
-end
-local numChars = {['e']=true; ['E']=true; ['+']=true; ['-']=true; ['.']=true}
-local function parseNumber(str)
-    local i = 1
-    while numChars[str:sub(i, i)] or tonumber(str:sub(i, i)) do
-        i = i + 1
-    end
-    local val = tonumber(str:sub(1, i - 1))
-    str = removeWhite(str:sub(i))
-    return val, str
-end
-local function parseString(str)
-    str = str:sub(2)
-    local s = ""
-    while str:sub(1,1) ~= "\"" do
-        local next = str:sub(1,1)
-        str = str:sub(2)
-        assert(next ~= "\n", "Unclosed string")
- 
-        if next == "\\" then
-            local escape = str:sub(1,1)
-            str = str:sub(2)
- 
-            next = assert(decodeControls[next..escape], "Invalid escape character")
-        end
- 
-        s = s .. next
-    end
-    return s, removeWhite(str:sub(2))
-end
-local parseValue, parseMember
-local function parseArray(str)
-    str = removeWhite(str:sub(2))
-    local val = {}
-    local i = 1
-    while str:sub(1, 1) ~= "]" do
-        local v = nil
-        v, str = parseValue(str)
-        val[i] = v
-        i = i + 1
-        str = removeWhite(str)
-    end
-    str = removeWhite(str:sub(2))
-    return val, str
-end
-local function parseObject(str)
-    str = removeWhite(str:sub(2))
-    local val = {}
-    while str:sub(1, 1) ~= "}" do
-        local k, v = nil, nil
-        k, v, str = parseMember(str)
-        val[k] = v
-        str = removeWhite(str)
-    end
-    str = removeWhite(str:sub(2))
-    return val, str
-end
-function parseMember(str)
-    local k = nil
-    k, str = parseValue(str)
-    local val = nil
-    val, str = parseValue(str)
-    return k, val, str
-end
-function parseValue(str)
-    local fchar = str:sub(1, 1)
-    if fchar == "{" then
-        return parseObject(str)
-    elseif fchar == "[" then
-        return parseArray(str)
-    elseif tonumber(fchar) ~= nil or numChars[fchar] then
-        return parseNumber(str)
-    elseif str:sub(1, 4) == "true" or str:sub(1, 5) == "false" then
-        return parseBoolean(str)
-    elseif fchar == "\"" then
-        return parseString(str)
-    elseif str:sub(1, 4) == "null" then
-        return parseNull(str)
-    end
-    return nil
-end
-local function decode(str)
-    str = removeWhite(str)
-    t = parseValue(str)
-    return t
+if #args < 2 then
+		print("Usage:\n"..((shell and shell.getRunningProgram()) or "gitget").." <user> <repo> [branch/tree] [path]") error()
 end
  
-local writeToFile = function(filename,contents)
-    local file = fs.open(filename,"w")
-    file.write(contents)
-    file.close()
+local function save(data,file)
+	local file = shell.resolve(file:gsub("%%20"," "))
+	if not (fs.exists(string.sub(file,1,#file - #fs.getName(file))) and fs.isDir(string.sub(file,1,#file - #fs.getName(file)))) then
+		if fs.exists(string.sub(file,1,#file - #fs.getName(file))) then fs.delete(string.sub(file,1,#file - #fs.getName(file))) end
+		fs.makeDir(string.sub(file,1,#file - #fs.getName(file)))
+	end
+	local f = fs.open(file,"w")
+	f.write(data)
+	f.close()
 end
  
-local getFromGitHub
-getFromGitHub = function(reponame,repopath,filepath,verbose)
-    local jason = http.get("https://api.github.com/repos/"..reponame.."/contents/"..(repopath or ""))
-    if not jason then return false end
-    local repo = decode(jason.readAll())
-    for k,v in pairs(repo) do
-        if v.message then
-            return false
-        else
-            if v.type == "file" then
-                if verbose then print("'"..fs.combine(filepath,v.name).."'") end
-                writeToFile(fs.combine(filepath,v.name),http.get(v.download_url).readAll())
-            elseif v.type == "dir" then
-                if verbose then print("'"..fs.combine(filepath,v.name).."'") end
-                fs.makeDir(fs.combine(filepath,v.name))
-                getFromGitHub(reponame,fs.combine(repopath,v.name),fs.combine(filepath,v.name),verbose)
-            end
-        end
-    end
+local function download(url, file)
+	save(http.get(url).readAll(),file)
 end
 
-term.clear()
-term.setCursorPos(1,1)
-
-getFromGitHub(reponame,repopath,outpath,verbose)
-
-term.clear()
-
-shell.run(fs.combine(outpath,"startup"))
+if not json then
+	download("http://pastebin.com/raw.php?i=4nRg9CHU","json")
+	os.loadAPI("json")
+end
+ 
+preset.start()
+local data = json.decode(http.get("https://api.github.com/repos/"..args[1].."/"..args[2].."/git/trees/"..args[3].."?recursive=1").readAll())
+if data.message and data.message:find("API rate limit exceeded") then error("Out of API calls, try again later") end
+if data.message and data.message == "Not found" then error("Invalid repository",2) else
+	for k,v in pairs(data.tree) do
+		-- Make directories
+		if v.type == "tree" then
+			fs.makeDir(fs.combine(args[4],v.path))
+			if not hide_progress then
+			end
+		end
+	end
+	local drawProgress
+	if async and not silent then
+		local _, y = term.getCursorPos()
+		local wide, _ = term.getSize()
+		term.setCursorPos(1, y)
+		term.write("[")
+		term.setCursorPos(wide - 6, y)
+		term.write("]")
+		drawProgress = function(done, max)
+			local value = done / max
+			term.setCursorPos(2,y)
+			term.write(("="):rep(math.floor(value * (wide - 8))))
+			local percent = math.floor(value * 100) .. "%"
+			term.setCursorPos(wide - percent:len(),y)
+			term.write(percent)
+		end
+	end
+	local filecount = 0
+	local downloaded = 0
+	local paths = {}
+	local failed = {}
+	for k,v in pairs(data.tree) do
+		-- Send all HTTP requests (async)
+		if v.type == "blob" then
+			v.path = v.path:gsub("%s","%%20")
+			local url = "https://raw.github.com/"..args[1].."/"..args[2].."/"..args[3].."/"..v.path,fs.combine(args[4],v.path)
+			if async then
+				http.request(url)
+				paths[url] = fs.combine(args[4],v.path)
+				filecount = filecount + 1
+			else
+				download(url, fs.combine(args[4], v.path))
+				if not silent then print(fs.combine(args[4], v.path)) end
+			end
+		end
+	end
+	while downloaded < filecount do
+		local e, a, b = os.pullEvent()
+		if e == "http_success" then
+			save(b.readAll(),paths[a])
+			downloaded = downloaded + 1
+			if not silent then drawProgress(downloaded,filecount) end
+		elseif e == "http_failure" then
+			-- Retry in 3 seconds
+			failed[os.startTimer(3)] = a
+		elseif e == "timer" and failed[a] then
+			http.request(failed[a])
+		end
+	end
+end
+preset.done(args[4])
